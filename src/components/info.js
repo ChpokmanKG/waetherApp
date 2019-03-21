ec2b8ed7a5b7f7eaf7243a4a2229d0e4992def4d
ChpokmanKG <39056174+ChpokmanKG@users.noreply.github.com>
2019-03-21 15:09:24 +0600

    Мелкие изменение

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Info = () => (
-      <div>
-        <h2>Погодное приложение</h2>
-        <p>Узнайте погоду в вашем городе!</p>
-      </div>
+  <div>
+    <h2>Chpokman Weather</h2>
+    <p>Узнайте погоду в вашем городе!</p>
+  </div>
 )
 
 export default Info;
